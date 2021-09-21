3f246ea1b8f9463cdfbc26cce7fe904fa6bf8751
LukinhaXTS <lucas_costasouza@yahoo.com.br>
2021-09-21 15:02:05 -0300

    Inicio de TypeScript

---
+++ TypeScript/index.tsx
@@ -0,0 +1,19 @@
+const bool : boolean = true;
+const numero : number = 200;
+const texto : string = "Lucas"
+
+const alunos : string [] = ["Julio", "Flávia", "Lucas", "Pedro Henrique", "Maria Clara"]
+
+const alunoNota : [string, number] = ["Lucas", 10]
+
+function mult(arg1 : number, arg2 : number) : void {
+
+    console.log(arg1*arg2)
+}
+
+function multi(arg1 : number, arg2 : number) : number {
+
+    console.log(arg1*arg2)
+
+    return arg1*arg2
+}
